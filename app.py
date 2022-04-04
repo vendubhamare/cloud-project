6bab568f92ad430a30166bd46811bd8c43dd9733
vendubhamare <69428037+vendubhamare@users.noreply.github.com>
2022-04-04 22:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = pickle.load(open('models/model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/quiz')
